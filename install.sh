84dc3cab5867bf652bccdd69c7746c8065fa7eb2
VISHNU PREMIL <82073583+strangedreamer4@users.noreply.github.com>
2024-01-07 08:34:47 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,19 +12,18 @@ wget https://cyberprime.netlify.app/mod.sh -O mod.sh && chmod +x mod.sh && sudo 
 # Check if espeak is already installed
 if command -v espeak &> /dev/null; then
     echo "espeak is already installed. Exiting."
-    exit 0
-fi
-
-# Install espeak using apt
-apt update
-apt install -y espeak
-
-# Check if the installation was successful
-if [ $? -eq 0 ]; then
-    echo "espeak has been successfully installed."
-    
-    # Run the Python script with sudo
-    sudo python vchat.py
 else
-    echo "Error: Failed to install espeak."
+    # Install espeak using apt
+    apt update
+    apt install -y espeak
+
+    # Check if the installation was successful
+    if [ $? -eq 0 ]; then
+        echo "espeak has been successfully installed."
+        
+        # Run the Python script with sudo
+        python3 vchat.py
+    else
+        echo "Error: Failed to install espeak."
+    fi
 fi
