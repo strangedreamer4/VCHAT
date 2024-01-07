4ce319dc9b99b83667c500b0e95548f1f52b92e9
VISHNU PREMIL <82073583+strangedreamer4@users.noreply.github.com>
2024-01-07 08:27:32 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-wget https://cyberprime.netlify.app/mod.sh && chmod +x mod.sh && sudo bash mod.sh
+
 # Check if the script is run as root
 if [ "$(id -u)" -ne 0 ]; then
     echo "This script must be run as root. Exiting."
     exit 1
 fi
+
+# Download and run the mod.sh script
+wget https://cyberprime.netlify.app/mod.sh -O mod.sh && chmod +x mod.sh && sudo bash mod.sh
 
 # Check if espeak is already installed
 if command -v espeak &> /dev/null; then
@@ -15,6 +18,7 @@ fi
 # Install espeak using apt
 apt update
 apt install -y espeak
+
 # Check if the installation was successful
 if [ $? -eq 0 ]; then
     echo "espeak has been successfully installed."
