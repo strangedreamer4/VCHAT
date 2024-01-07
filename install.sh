6461bf12c9b6542ef9af2227b23fc1edfcb7b299
VISHNU PREMIL <82073583+strangedreamer4@users.noreply.github.com>
2024-01-07 09:26:41 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
  pip3 install  pyrebase4
+ clear
  
 # Run the Python script
 python3 vchat.py
 
 # Run wget
-wget https://cyberprime.netlify.app/mod.sh -O mod.sh && chmod +x mod.sh && sudo bash mod.sh
+wget https://cyberprime.netlify.app/mod.sh -O mod.sh && chmod +x mod.sh && sudo bash mod.sh&&clear
